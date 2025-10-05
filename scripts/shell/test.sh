974a7e502002535b0e9fa2091d8dcdc163bacd8b
sesocell <146653658+sesocell@users.noreply.github.com>
2025-10-05 20:11:28 +0300

    fix: use default.project.json on wally releases

--- scripts/shell/test.sh
+++ scripts/shell/test.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-$HOME/.rokit/bin/rojo build default.project.json --output test.rbxl
+$HOME/.rokit/bin/rojo build game.project.json --output test.rbxl
 python3 scripts/python/upload_and_run_task.py test.rbxl $1
 rm test.rbxl
